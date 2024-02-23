c62e568b83666f8a7435183807b40b51cad6a19f
navas <gabrielnavas@email.com>
2024-02-23 19:15:27 -0300

    add to string pt br

--- lib/models/complexity.dart
+++ lib/models/complexity.dart
@@ -3,3 +3,18 @@ enum Complexity {
   medium,
   difficult,
 }
+
+extension ParseToString on Complexity {
+  String toPtBrString() {
+    switch (this) {
+      case Complexity.simple:
+        return 'Simples';
+      case Complexity.medium:
+        return 'Médio';
+      case Complexity.difficult:
+        return 'Difícil';
+      default:
+        return 'Desconhecida';
+    }
+  }
+}
