d3023cd2fb1e1bbf26c2dff269d4ccb3292c8463
navas <gabrielnavas@email.com>
2024-02-23 01:15:11 -0300

    get just meal with category selected

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:meals_flutter_app/data/dummy_data.dart';
 import 'package:meals_flutter_app/models/category.dart';
+import 'package:meals_flutter_app/models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Category category =
         ModalRoute.of(context)!.settings.arguments as Category;
+
+    List<Meal> meals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -14,8 +20,8 @@ class CategoriesMealsScreen extends StatelessWidget {
           category.title,
         ),
       ),
-      body: Center(
-        child: Text('Receitas por Categoria ${category.title}'),
+      body: Column(
+        children: meals.map((meal) => Text(meal.title)).toList(),
       ),
     );
   }
